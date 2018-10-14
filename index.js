3554410b541e044a47d6c627bb0fd0a9fdd09e48
BetoRincon <aluisrincon@unisangil.edu.co>
2018-10-14 02:58:04 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("listo");
+
+
+fetch('https://bdadmin.000webhostapp.com/wp-json/wp/v2/posts?order=asc&_embed')
+    .then(response => {
+        return response.json();
+    })
+    .then(myJson => {
+        console.log(myJson);
+        myJson.forEach(element => {
+            var list = document.getElementById('list');
+
+            let ilist = document.createElement('div')
+            let post = document.createElement('a');
+            // let aTag = document.createElement('a');
+            post.setAttribute('href', '#' + element.title.rendered.split(" ").join('-'));
+            // post.appendChild(aTag);
+            post.innerHTML = element.title.rendered;
+            ilist.appendChild(post);
+            ilist.setAttribute("id", "list-item");
+            document.getElementById('list').appendChild(ilist);
+            list.appendChild(ilist);
+        });
+    })
